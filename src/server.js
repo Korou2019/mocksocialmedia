b0ca23f446e280e5b70f11b447ada8f6fd81e2d8
Korou <koroudakshana@gmail.com>
2021-06-26 00:05:57 +0530

    server update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express=require('express')
 const { db } = require('./db/models')
 
 const app=express()
-
+const SERVER_PORT=process.env.PORT || 8383
 
 const {usersroute}=require('./routes/users')
 const {postsroute}=require('./routes/posts')
@@ -20,7 +20,7 @@ app.use('/',express.static(__dirname +'/public'))
 const task=async() =>{
       try{
           await db.sync({alter:true})
-          app.listen(8383,() =>{
+          app.listen(SERVER_PORT,() =>{
               console.log('Server started on http://localhost:8383')
           })
 
